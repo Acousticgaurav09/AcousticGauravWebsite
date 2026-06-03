b8c045f24bb17cad06c3af86093cc0e44c906167
acousticgaurav09 <gauravonly99@gmail.com>
2026-06-03 21:09:34 +0530

    changes 5

--- artifacts/acoustic-gaurav/src/App.tsx
+++ artifacts/acoustic-gaurav/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import LoadingScreen from "@/components/LoadingScreen";
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 import Stats from "@/components/Stats";
 import About from "@/components/About";
 import Services from "@/components/Services";
